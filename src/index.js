b629b8c78b250f470b5597cf4d87b599a1a4e159
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-05-26 04:00:47 +0430

    post the form data in game scores and fetch the response from server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import './style.css';
 
-const gameId = LYADjurhsRv39Hwpf4Ya;
+const gameId = 'LYADjurhsRv39Hwpf4Ya';
 const gameForm = document.getElementById('gameForm');
+gameForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const user = document.getElementById('userName');
+  const score = document.getElementById('userScore');
+  const gameFormData = {
+    user: user.value,
+    score: parseInt(score.value),
+  };
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`, {
+    method: 'POST',
+    body: JSON.stringify(gameFormData),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+  const result = await response.json();
+  console.log(result);
+})
